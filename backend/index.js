bdf4e68951147fe95ebbe8d42277a24ff59c01c5
Krishna-Joshi09 <krishnabj94@gmail.com>
2024-04-04 19:32:33 +0530

    create-product

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+const express = require("express"); // creating the server
+const app = express();
+app.use(express.json);
+const mongoose = require("mongoose"); // connecting to database
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/production")
+  .then((sucess) => {
+    console.log("Database is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+app.listen(3000, (err, success) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Server is running on port=>"+3000);
+  }
+}); //where my services will called()
+
+const productRoutes = require("./routes/productRoutes");
+app.use("/products", productRoutes);
